bf3f7e19f8f76bc7aaa519c3712b42c02ea2e3d0
Norman <ncroan@gmail.com>
2016-06-23 17:06:59 -0400

    updated click to respond to body

--- public/js/adventure.js
+++ public/js/adventure.js
@@ -33,7 +33,7 @@ function showQuest(id) {
   const delay = 45;
   const elem = $('#questText');
   const addTextByDelay = function (text, elem, delay) {
-    $('#questText').click(function () {
+    $('body').click(function () {
       console.log("changing delay");
       delay = 5;
     });
