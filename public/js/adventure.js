a60e09728f7cee89a2449cd148e8dcc02091858f
Norman <ncroan@gmail.com>
2016-06-23 16:52:41 -0400

    disabled workers and updated css

--- public/js/adventure.js
+++ public/js/adventure.js
@@ -1,4 +1,4 @@
-if ('serviceWorker' in navigator) {
+/*if ('serviceWorker' in navigator) {
  console.log('Service Worker is supported');
  navigator.serviceWorker.register('../sw.js').then(function(reg) {
    console.log(':^)', reg);
@@ -7,6 +7,7 @@ if ('serviceWorker' in navigator) {
    console.log(':^(', err);
  });
 }
+*/
 
 function loadJSON(callback) {
   const xobj = new XMLHttpRequest();
