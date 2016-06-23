3fd5804139c37de576645163ffc85aadccaf0b0d
Norman <ncroan@gmail.com>
2016-06-23 16:24:12 -0400

    added service worker

--- public/js/adventure.js
+++ public/js/adventure.js
@@ -1,4 +1,12 @@
-var quest = [];
+if ('serviceWorker' in navigator) {
+ console.log('Service Worker is supported');
+ navigator.serviceWorker.register('../sw.js').then(function(reg) {
+   console.log(':^)', reg);
+   // TODO
+ }).catch(function(err) {
+   console.log(':^(', err);
+ });
+}
 
 function loadJSON(callback) {
   const xobj = new XMLHttpRequest();
